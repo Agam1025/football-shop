8d0c110e35a88be6280e17e8feede09910846626
Agam1025 <naufalagam59070@gmail.com>
2025-09-10 08:32:02 +0700

    ubah npm menjadi nama aplikasi

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'npm' : '2406345974',
+        'applicationname' : 'Football Shop',
         'name': 'Naufal Agam Ardiansyah',
         'class': 'PBP B'
     }
