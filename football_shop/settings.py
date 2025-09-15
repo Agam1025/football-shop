ef701ed50508e08764b51fae80af2a26da45aae9
Agam1025 <naufalagam59070@gmail.com>
2025-09-15 14:14:58 +0700

    Commit Ulang

--- football_shop/settings.py
+++ football_shop/settings.py
@@ -34,8 +34,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "naufal-agam-footballshop.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://naufal-agam-footballshop.pbp.cs.ui.ac.id/",
-    "http://naufal-agam-footballshop.pbp.cs.ui.ac.id/"
+    "https://naufal-agam-footballshop.pbp.cs.ui.ac.id",
 ]
 
 # Application definition
